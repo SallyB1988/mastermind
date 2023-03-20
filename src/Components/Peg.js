cd54586e7c0aa46af4148a45d63ce223d32b494f
Sally Beckwith <sallypbeckwith@gmail.com>
2023-03-20 13:25:03 -0400

    testing git

--- src/Components/Peg.js
+++ src/Components/Peg.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { GameContext } from "./Gameboard";
 import { Icon } from "semantic-ui-react";
 
+// adding a comment
 const Peg = ({ color, size = 30 }) => {
   const { selectColor } = useContext(GameContext);
 
